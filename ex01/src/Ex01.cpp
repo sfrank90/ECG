692513c1291e0a803b9f0cff0697c060b9d435ab
Sven <sven.frank90@gmail.com>
2014-11-10 11:48:18 +0100

    comments for ex01 and init commit for ex02

--- ex01/src/Ex01.cpp
+++ ex01/src/Ex01.cpp
@@ -5,20 +5,29 @@
 
 void triangleSolution(void) {
   // TODO: implement your solution here //
+	
+	//definition of vectors
 	glm::vec3 vec_a = glm::vec3(4.0, 1.0, 0.0);
 	glm::vec3 vec_b = glm::vec3(-1.0, 3.0, 2.0);
 	glm::vec3 vec_c = glm::vec3(0.0, 2.0, 1.0);
 
 	glm::vec3 vec_v = glm::vec3(0.0, 2.0, 1.0);
 
+	//calc of vectors
 	glm::vec3 vec_ab = vec_b - vec_a;
 	glm::vec3 vec_ac = vec_c - vec_a;
 
+	//normal of triangle
 	glm::vec3 triangle_n = glm::cross(vec_ab, vec_ac);
 
+	//normalize
 	glm::vec3 tn_n = glm::normalize(triangle_n);
 	glm::vec3 vec_vn = glm::normalize(vec_v);
+
+	// angle between normal and given vector
 	float angle = glm::acos(glm::dot(tn_n, vec_vn));
+
+	// conversion into degress
 	angle = angle * 180 / M_PI;
 	printf("Angle: %f\n", angle);
   
---
+++ ex02/include/Ex02.h
@@ -0,0 +1,20 @@
+#ifndef __EX02__
+#define __EX02__
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/string_cast.hpp> 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+// include bunny geometry //
+//#include "bunny.h"
+
+#endif
